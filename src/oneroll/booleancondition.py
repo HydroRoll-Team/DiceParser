bd404f75c283c36ba9a483b09e7282f0e33f7305
简律纯 <i@jyunko.cn>
2023-10-05 05:53:49 +0800

    feat(node, test): add nodes

--- src/oneroll/booleancondition.py
+++ src/oneroll/booleancondition.py
@@ -1,142 +1,165 @@
+from typing import List, Tuple
+from diceparser.diceparserhelper import Die
+from node.executionnode import ExecutionNode
+from validator import Validator
 from enum import Enum
-from typing import Tuple
+
+class CompareOperator(Enum):
+    Equal = "="
+    GreaterThan = ">"
+    LesserThan = "<"
+    GreaterOrEqual = ">="
+    LesserOrEqual = "<="
+    Different = "!="
 
 class CONDITION_STATE(Enum):
     UNREACHABLE = 0
     ALWAYSTRUE = 1
     REACHABLE = 2
 
-class CompareOperator(Enum):
-    Equal = 0
-    GreaterThan = 1
-    LesserThan = 2
-    GreaterOrEqual = 3
-    LesserOrEqual = 4
-    Different = 5
+def testEqual(insideRange: bool, range: Tuple[int, int]) -> CONDITION_STATE:
+    if not insideRange:
+        return CONDITION_STATE.UNREACHABLE
+    elif insideRange and (range[0] == range[1]):
+        return CONDITION_STATE.ALWAYSTRUE
+    else:
+        return CONDITION_STATE.REACHABLE
 
-class ExecutionNode:
+def testGreatherThan(value: int, range: Tuple[int, int]) -> CONDITION_STATE:
+    if value >= max(range[0], range[1]):
+        return CONDITION_STATE.UNREACHABLE
+    elif value < min(range[0], range[1]):
+        return CONDITION_STATE.ALWAYSTRUE
+    else:
+        return CONDITION_STATE.REACHABLE
+
+def testLesserThan(value: int, range: Tuple[int, int]) -> CONDITION_STATE:
+    if value <= min(range[0], range[1]):
+        return CONDITION_STATE.UNREACHABLE
+    elif value > max(range[0], range[1]):
+        return CONDITION_STATE.ALWAYSTRUE
+    else:
+        return CONDITION_STATE.REACHABLE
+
+def testGreaterOrEqual(value: int, range: Tuple[int, int]) -> CONDITION_STATE:
+    if value > max(range[0], range[1]):
+        return CONDITION_STATE.UNREACHABLE
+    elif value <= min(range[0], range[1]):
+        return CONDITION_STATE.ALWAYSTRUE
+    else:
+        return CONDITION_STATE.REACHABLE
+
+def testLesserOrEqual(value: int, range: Tuple[int, int]) -> CONDITION_STATE:
+    if value < min(range[0], range[1]):
+        return CONDITION_STATE.UNREACHABLE
+    elif value >= max(range[0], range[1]):
+        return CONDITION_STATE.ALWAYSTRUE
+    else:
+        return CONDITION_STATE.REACHABLE
+
+def testDifferent(inside: bool, range: Tuple[int, int]) -> CONDITION_STATE:
+    if inside and (range[0] == range[1]):
+        return CONDITION_STATE.UNREACHABLE
+    elif not inside:
+        return CONDITION_STATE.ALWAYSTRUE
+    else:
+        return CONDITION_STATE.REACHABLE
+
+class BooleanCondition(Validator):
     def __init__(self):
-        self.result = None
+        super().__init__()
+        self.m_operator = CompareOperator.Equal
+        self.m_value = None
 
-    def run(self, context):
-        # Run the execution node logic and set the result
-        pass
+    def __del__(self):
+        if self.m_value is not None:
+            del self.m_value
+            self.m_value = None
 
-    def get_result(self):
-        return self.result
-
-class BooleanCondition:
-    def __init__(self):
-        self.operator = CompareOperator.Equal
-        self.value = None
-
-    def test_equal(self, inside_range: bool, range: Tuple[int, int]) -> CONDITION_STATE:
-        if not inside_range:
-            return CONDITION_STATE.UNREACHABLE
-        elif inside_range and (range[0] == range[1]):
-            return CONDITION_STATE.UNREACHABLE
-        elif value < min(range[0], range[1]):
-            return CONDITION_STATE.ALWAYSTRUE
-        else:
-            return CONDITION_STATE.REACHABLE
-
-    def test_different(self, inside: bool, range: Tuple[int, int]) -> CONDITION_STATE:
-        if not inside:
-            return CONDITION_STATE.UNREACHABLE
-        elif inside and (range[0] == range[1]):
-            return CONDITION_STATE.UNREACHABLE
-        elif value > max(range[0], range[1]):
-            return CONDITION_STATE.UNREACHABLE
-        elif value <= min(range[0], range[1]):
-            return CONDITION_STATE.ALWAYSTRUE
-        else:
-            return CONDITION_STATE.REACHABLE
-
-    def has_valid(self, b, recursive, unhighlight) -> int:
-        list_values = []
-        if self.condition_type == OnEachValue:
-            list_values.append(b.get_value())
+    def hasValid(self, b: Die, recursive: bool, unhighlight: bool) -> int:
+        listValues = []
+        if self.m_conditionType == Dice.OnEachValue:
+            listValues.append(b.getValue())
         elif recursive:
-            list_values = b.get_list_value()
+            listValues = b.getListValue()
         else:
-            list_values.append(b.get_last_rolled_value())
+            listValues.append(b.getLastRolledValue())
 
         sum = 0
-        value_scalar = self.value_to_scalar()
-        for value in list_values:
-            if self.operator == CompareOperator.Equal:
-                sum += 1 if value == value_scalar else 0
-            elif self.operator == CompareOperator.GreaterThan:
-                sum += 1 if value > value_scalar else 0
-            elif self.operator == CompareOperator.LesserThan:
-                sum += 1 if value < value_scalar else 0
-            elif self.operator == CompareOperator.GreaterOrEqual:
-                sum += 1 if value >= value_scalar else 0
-            elif self.operator == CompareOperator.LesserOrEqual:
-                sum += 1 if value <= value_scalar else 0
-            elif self.operator == CompareOperator.Different:
-                sum += 1 if value != value_scalar else 0
+        valueScalar = self.valueToScalar()
+        for value in listValues:
+            if self.m_operator == CompareOperator.Equal:
+                sum += 1 if value == valueScalar else 0
+            elif self.m_operator == CompareOperator.GreaterThan:
+                sum += 1 if value > valueScalar else 0
+            elif self.m_operator == CompareOperator.LesserThan:
+                sum += 1 if value < valueScalar else 0
+            elif self.m_operator == CompareOperator.GreaterOrEqual:
+                sum += 1 if value >= valueScalar else 0
+            elif self.m_operator == CompareOperator.LesserOrEqual:
+                sum += 1 if value <= valueScalar else 0
+            elif self.m_operator == CompareOperator.Different:
+                sum += 1 if value != valueScalar else 0
 
         if unhighlight and sum == 0:
-            b.set_highlighted(False)
+            b.setHighlighted(False)
         else:
-            b.set_highlighted(True)
+            b.setHighlighted(True)
 
         return sum
 
-    def set_operator(self, operator):
-        self.operator = operator
+    def setOperator(self, m: CompareOperator):
+        self.m_operator = m
 
-    def set_value_node(self, value_node):
-        self.value = value_node
+    def setValueNode(self, v: ExecutionNode):
+        self.m_value = v
 
-    def to_string(self):
+    def toString(self) -> str:
         str = ""
-        if self.operator == CompareOperator.Equal:
+        if self.m_operator == CompareOperator.Equal:
             str += "="
-        elif self.operator == CompareOperator.GreaterThan:
+        elif self.m_operator == CompareOperator.GreaterThan:
             str += ">"
-        elif self.operator == CompareOperator.LesserThan:
+        elif self.m_operator == CompareOperator.LesserThan:
             str += "<"
-        elif self.operator == CompareOperator.GreaterOrEqual:
+        elif self.m_operator == CompareOperator.GreaterOrEqual:
             str += ">="
-        elif self.operator == CompareOperator.LesserOrEqual:
+        elif self.m_operator == CompareOperator.LesserOrEqual:
             str += "<="
-        elif self.operator == CompareOperator.Different:
+        elif self.m_operator == CompareOperator.Different:
             str += "!="
-        return "[{}{}]".format(str, self.value_to_scalar())
+        return "[{}{}]".format(str, self.valueToScalar())
 
-    def is_valid_range_size(self, range: Tuple[int, int]) -> CONDITION_STATE:
-        value_scalar = self.value_to_scalar()
-        bound_value = min(max(range[0], value_scalar), range[1])
-        is_inside_range = bound_value == value_scalar
+    def isValidRangeSize(self, range: Tuple[int, int]) -> CONDITION_STATE:
+        valueScalar = self.valueToScalar()
+        boundValue = max(range[0], min(valueScalar, range[1]))
+        isInsideRange = boundValue == valueScalar
+        if self.m_operator == CompareOperator.Equal:
+            return testEqual(isInsideRange, range)
+        elif self.m_operator == CompareOperator.GreaterThan:
+            return testGreatherThan(valueScalar, range)
+        elif self.m_operator == CompareOperator.LesserThan:
+            return testLesserThan(valueScalar, range)
+        elif self.m_operator == CompareOperator.GreaterOrEqual:
+            return testGreaterOrEqual(valueScalar, range)
+        elif self.m_operator == CompareOperator.LesserOrEqual:
+            return testLesserOrEqual(valueScalar, range)
+        elif self.m_operator == CompareOperator.Different:
+            return testDifferent(isInsideRange, range)
 
-        if self.operator == CompareOperator.Equal:
-            return self.test_equal(is_inside_range, range)
-        elif self.operator == CompareOperator.GreaterThan:
-            return self.test_greater_than(value_scalar, range)
-        elif self.operator == CompareOperator.LesserThan:
-            return self.test_lesser_than(value_scalar, range)
-        elif self.operator == CompareOperator.GreaterOrEqual:
-            return self.test_greater_or_equal(value_scalar, range)
-        elif self.operator == CompareOperator.LesserOrEqual:
-            return self.test_lesser_or_equal(value_scalar, range)
-        elif self.operator == CompareOperator.Different:
-            return self.test_different(is_inside_range, range)
-
-    def get_copy(self):
+    def getCopy(self) -> Validator:
         val = BooleanCondition()
-        val.set_operator(self.operator)
-        val.set_value_node(self.value.get_copy())
+        val.setOperator(self.m_operator)
+        val.setValueNode(self.m_value.getCopy())
         return val
 
-    def value_to_scalar(self):
-        if self.value is None:
+    def valueToScalar(self) -> int:
+        if self.m_value is None:
             return 0
 
-        self.value.run(None)
-        result = self.value.get_result()
+        self.m_value.run(None)
+        result = self.m_value.getResult()
         if result:
-            return int(result.get_result(Dice.RESULT_TYPE.SCALAR))
+            return int(result.getResult(Dice.RESULT_TYPE.SCALAR))
         else:
             return 0
