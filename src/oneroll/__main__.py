e3fe3ab98847d83206fd5489e586ad0b2783c4fc
简律纯 <i@jyunko.cn>
2023-11-30 16:01:10 +0800

    feat(cli): add cli parser

--- src/oneroll/__main__.py
+++
@@ -1,5 +0,0 @@
-from oneroll import roll
-
-while True:
-    roll_result = roll(input(), allow_comments=True)
-    print(str(roll_result))
--- tests/dice/rd.py
+++ tests/dice/rd.py
@@ -1,2 +1,3 @@
 import oneroll
 
+print(oneroll.roll('2d4+2 # ddd', allow_comments=True).comment)
